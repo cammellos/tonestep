197fbf37c525544c05d8dfc394b25392ce364546
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2024-09-30 16:51:27 +0100

    feat(android): load ndk

--- flake.nix
+++ flake.nix
@@ -1,79 +1,37 @@
 {
-  description = "A basic project in flutter";
+  description = "Flake for Android development with Rust and Cargo APK";
 
   inputs = {
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    android-nixpkgs = {
+     android-nixpkgs = {
       url = "github:tadfisher/android-nixpkgs";
     };
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # or a specific version/branch you prefer
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat, android-nixpkgs }:
-    flake-utils.lib.eachDefaultSystem ( system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            android_sdk = {
-              accept_license = true;
-            };
-          };
+  outputs = { self, nixpkgs, android-nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system;
+
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
         };
-        #androidEnvCustomPackage10 = pkgs.androidenv.androidPkgs_9_0;
-        #androidEnvCustomPackage3 = pkgs.androidenv.composeAndroidPackages {
-        #  buildToolsVersions = [ "34.0.0" "28.0.3" ];
-        #  platformVersions = [ "34" "28" ];
-        #  abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-        #};
-        #androidEnvCustomPackage2 = pkgs.androidenv.composeAndroidPackages {};
-        androidEnvCustomPackage = pkgs.androidenv.composeAndroidPackages {
-          # cmdLineToolsVersion = 11.0;
-          toolsVersion = "26.1.1";
-          platformToolsVersion = "34.0.5";
-          buildToolsVersions = [ "30.0.3" "34.0.0" ];
-          includeEmulator = true;
-          emulatorVersion = "34.1.9";
-          platformVersions = [ "28" "29" "30" "31" "32" "33" "34" ];
-          includeSources = false;
-          includeSystemImages = false;
-          systemImageTypes = [ "google_apis_playstore" ];
-          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-          cmakeVersions = [ "3.10.2" ];
-          includeNDK = true;
-          ndkVersions = [ "22.0.7026061" ];
-          useGoogleAPIs = false;
-          useGoogleTVAddOns = false;
-        };
-        androidCustomPackage = android-nixpkgs.sdk.${system} (
-          sdkPkgs: with sdkPkgs; [
-            cmdline-tools-latest
-            build-tools-30-0-3
-            build-tools-33-0-2
-            build-tools-34-0-0
-            platform-tools
-            emulator
-            #patcher-v4
-            platforms-android-28
-            platforms-android-29
-            platforms-android-30
-            platforms-android-31
-            platforms-android-32
-            platforms-android-33
-            platforms-android-34
-          ]
-        );
-        pinnedJDK = pkgs.jdk17; # jdk11, jdk13
-      in {
-        devShells = {
-          default = pkgs.mkShell {
-            name = "ear-trainer-nix-shell";
+
+      };
+
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        abiVersions = [ "arm64-v8a" "x86_64" ];
+        includeNDK = true;
+        platformVersions = [ "30" ];
+      };
+
+    in {
+      devShell = pkgs.mkShell {
+        # Workaround for https://github.com/NixOS/nixpkgs/issues/60919.
+        hardeningDisable = [ "fortify" ];
+
+        # Allow cargo to download crates (even inside `nix-shell --pure`).
             buildInputs = with pkgs; [
               flutter
               pkg-config
@@ -93,31 +51,27 @@
               alsa-lib
               pcre2.dev
               rustup
-              android-studio
-            ] ++ [
-              pinnedJDK
-              #androidEnvCustomPackage.androidsdk
-              androidCustomPackage
             ];
-            shellHook = ''
-            #  GRADLE_USER_HOME=$HOME/gradle-user-home
-            #  GRADLE_HOME=$HOME/gradle-home
-            export LD_LIBRARY_PATH="$(pwd)/eartrainer/build/linux/x64/debug/bundle/lib:$LD_LIBRARY_PATH"
-              cargo install flutter_rust_bridge_codegen
-              export PATH="$HOME/.cargo/bin:$PATH"
-            '';
-            JAVA_HOME = pinnedJDK;
-            #ANDROID_HOME = "${androidEnvCustomPackage.androidsdk}/libexec/android-sdk";
-            ANDROID_HOME = "${androidCustomPackage}/share/android-sdk";
-            #ANDROID_SDK_HOME = "${androidCustomPackage}/share/android-sdk";
-            #ANDROID_SDK_ROOT = "${androidCustomPackage}/share/android-sdk";
-            #ANDROID_AVD_HOME = (toString ./.) + "/.android/avd";
-            #GRADLE_USER_HOME = "/home/specx/gradle-user-home";
-            GRADLE_USER_HOME = "/home/specx/.gradle";
-            #GRADLE_HOME = "/home/specx/gradle-home";
-            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidCustomPackage}/share/android-sdk/build-tools/34.0.0/aapt2";
-          };
-        };
-      }
-    );
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.rustup
+          pkgs.cargo-apk
+          pkgs.jdk
+          pkgs.alsa-lib
+        ];
+
+        shellHook = ''
+          export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+          export ANDROID_SDK_ROOT="${androidComposition.androidsdk}/libexec/android-sdk"
+          export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/ndk-bundle"
+          export PATH="$HOME/.cargo/bin:$PATH"
+
+          export LD_LIBRARY_PATH="$(pwd)/eartrainer/build/linux/x64/debug/bundle/lib:$LD_LIBRARY_PATH"
+
+          cargo install flutter_rust_bridge_codegen
+        '';
+
+      };
+    });
 }
